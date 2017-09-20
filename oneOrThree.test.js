7d54f3c5d38f4ca58091ab4a611e163d950e295d
Jeanine Schoessler <jeanine.mt@gmail.com>
2017-09-20 13:51:17 -0600

    add test for one or three

--- oneOrThree.test.js
+++ oneOrThree.test.js
@@ -4,21 +4,22 @@ var assert = chai.assert;
 var expect = chai.expect;
 
 //Write a JavaScript program to test if an array of integers of length 2 contains 1 or a 3
-var oneOrThree = function(arr) {
-    return arr;
+var oneOrThree = function (arr) {
+  return arr;
 }
-describe("oneOrThreeFunction", function(){
-    it ('is returning array', function(){
-        var val = oneOrThree([]);
-        val.should.be.a('array');
-	// var val = test([1,2,3]);
-	// expect(val).to.equal(true);
-    });
+describe("oneOrThreeFunction", function () {
+  it('is returning array', function () {
+    var val = oneOrThree([]);
+    val.should.be.a('array');
+  });
 
-    it ('has two elements', function(){ 
-        var val = oneOrThree([2,3]);
-        assert.lengthOf(val, 2); 
-	// var val = test([1,2,3]);
-	// expect(val).to.equal(true);
-    });
+  it('has two elements', function () {
+    var val = oneOrThree([2, 3]);
+    assert.lengthOf(val, 2);
+  });
+
+  it('val equals one or three', function () {
+    var val = oneOrThree([1, 3, 7]);
+    expect(val).to.equal(false);
+  });
 });
